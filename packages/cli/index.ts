3277da3c8bcfd9a20b45156b20b4e12dbcc25615
2113ic <icxiaoyanz@gmail.com>
2024-02-20 15:18:58 +0800

    feat: init cli package

--- packages/cli/index.ts
+++ packages/cli/index.ts
@@ -1,1 +1,38 @@
 #!/usr/bin/env node
+import { Command } from 'commander'
+import { version } from './package.json'
+
+const program = new Command()
+
+program
+  .name('blog')
+  .description('@icxy-blog command line tools')
+  .version(version)
+
+program.command('login')
+  .description('Connect to Supabase data & authorize login via GitHub')
+
+program.command('logout')
+  .description('Log out and clear authentication information')
+
+program.command('ui')
+  .description('Open the UI operation interface')
+  .option('-o, --open', 'Automatically open in the browser')
+  .option('-p, --port <port>', 'Specify the port number for the UI interface')
+
+program.command('add')
+  .description('Add a new article')
+  .option('-d, --draft', 'Add as a draft')
+  .option('-c, --category <category>', 'Specify the category of the article')
+
+program.command('del <articleId>')
+  .description('Delete an article')
+  .option('-f, --force', 'Force deletion without confirmation prompt')
+
+program.command('ls')
+  .description('List all articles (excluding drafts)')
+  .option('-a, --all', 'List all articles, including drafts')
+  .option('-d, --draft', 'List only draft articles')
+  .option('-c, --category <category>', 'Filter articles by category')
+
+program.parse()
---
+++ packages/cli/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: './index.ts',
+      formats: ['es', 'cjs'],
+      fileName: 'index',
+    },
+    rollupOptions: {
+      external: ['commander'],
+    },
+  },
+})
