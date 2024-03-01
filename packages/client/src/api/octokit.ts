de5fefed8fa91d79863e79324d6eafc0045f9ac5
2113ic <icxiaoyanz@gmail.com>
2024-03-01 11:16:30 +0800

    fix: use userStore accessToken

--- packages/client/src/api/octokit.ts
+++ packages/client/src/api/octokit.ts
@@ -1,6 +1,9 @@
 import { Octokit } from '@octokit/core'
+import { useUserStore } from '@/store/user'
+
+const userStore = useUserStore()
 
 export const HEADER_API = { 'X-GitHub-Api-Version': '2022-11-28' }
 export const octokit = new Octokit({
-  auth: import.meta.env.VITE_GITHUB_ACCESS_TOKEN,
+  auth: userStore.accessToken,
 })
